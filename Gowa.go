259cf9c5c6769dcf9a6bbe5db640ae9b88b3bf53
Pablo LM <pabloplm@gmail.com>
2016-10-22 21:31:42 +0200

    Added Function to add routes using a mux.Router

--- Gowa.go
+++ Gowa.go
@@ -4,6 +4,9 @@ import (
 	"runtime"
 	"path"
 	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"github.com/plopezm/goServerUtils"
 )
 
 func GowaStart(dbtype string, dbpath string, pageSize uint32) *GowaManager{
@@ -12,8 +15,23 @@ func GowaStart(dbtype string, dbpath string, pageSize uint32) *GowaManager{
 	return GM
 }
 
+func GowaAddRoutes(router *mux.Router) *mux.Router{
+	for _,route := range GM.GetRoutes(){
+		var handler http.Handler
+		handler = route.HandlerFunc
+		handler = goServerUtils.Logger(handler, route.Name)
+		fmt.Println("Adding route: "+route.Name+" -> "+route.Method+" "+route.Pattern);
+		router.
+		Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+	return router;
+}
+
 func GowaGetTemplatePath() string{
 	_, filename, _, _ := runtime.Caller(0)
-	fmt.Println(path.Join(path.Dir(filename), "template"))
+	//fmt.Println(path.Join(path.Dir(filename), "template"))
 	return path.Join(path.Dir(filename), "template")
 }
